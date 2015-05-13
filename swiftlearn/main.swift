d061735968c9c1ee99e9efee88db2e925553d4ab
fyhack <fyhack@qq.com>
2015-05-13 13:58:06 +0800

    Initial Commit

---
+++ swiftlearn/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftlearn
+//
+//  Created by Fyhack on 5/13/15.
+//  Copyright (c) 2015 Fyhack. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
